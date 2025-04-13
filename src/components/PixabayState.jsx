daf9123be94be2fae247604746df58aebd813bf0
Jhalendra Dewangan <108886766+acmejd@users.noreply.github.com>
2025-04-13 20:51:58 +0530

    Update PixabayState.jsx

--- src/components/PixabayState.jsx
+++ src/components/PixabayState.jsx
@@ -2,9 +2,11 @@ import React, { useEffect, useState } from 'react'
 import PixabayContext from './PixabayContext'
 
 const PixabayState = (props) => {
-  const api_key = "49679205-2c02a73b9591c4117cfd4fb09"
-  const [query, setQuery] = useState("india")
   const [imageData, setImageData] = useState([])
+  const [query, setQuery] = useState("india")
+
+  const api_key = "49679205-2c02a73b9591c4117cfd4fb09"
+  
   useEffect(() => {
     const fetchDataByAPI = async () => {
       const api = await fetch(`https://pixabay.com/api/?key=${api_key}&q=${query}&image_type=photo&pretty=true&per_page=100`)
@@ -24,6 +26,12 @@ const PixabayState = (props) => {
     setImageData(data.hits)
   }
 
+  // const fetchBySearch = async () => {
+    // const api = await fetch(`https://pixabay.com/api/?key=${api_key}&category=${cat}&image_type=photo&pretty=true&per_page=100`)
+    // const data = await api.json()
+    // console.log(data)
+    // setImageData(data.hits)
+  // }
 
   return (
     <>
@@ -32,7 +40,7 @@ const PixabayState = (props) => {
         query,
         setQuery,
         fetchImageByCategory,
-        fetchBySearch
+        // fetchBySearch
 
       }}>{props.children}</PixabayContext.Provider>
     </>
